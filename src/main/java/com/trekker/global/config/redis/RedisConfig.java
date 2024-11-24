bd66161215ff708e4d997056e8b1fee5f9f8a1ec
pcb7893@naver.com <122460524+chanbyoung@users.noreply.github.com>
2024-11-24 19:31:12 +0900

    feat: redis 설정 등록

---
+++ src/main/java/com/trekker/global/config/redis/RedisConfig.java
@@ -0,0 +1,33 @@
+package com.trekker.global.config.redis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(
+            RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        // 키를 문자열로 직렬화
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+        // JSON 직렬화기 추가 설정
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        // 해시 키를 문자열로 직렬화
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+
+        // 해시 값을 JSON 형식으로 직렬화
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        return redisTemplate;
+    }
+}

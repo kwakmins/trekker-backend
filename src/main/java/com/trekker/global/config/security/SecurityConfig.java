d49fe77b92c90d061859be6cc17325cf260732b9
pcb7893@naver.com <122460524+chanbyoung@users.noreply.github.com>
2024-11-24 23:10:42 +0900

    refactor: 구현 사항 security 설정 적용

--- src/main/java/com/trekker/global/config/security/SecurityConfig.java
+++ src/main/java/com/trekker/global/config/security/SecurityConfig.java
@@ -1,12 +1,21 @@
 package com.trekker.global.config.security;
 
+import com.trekker.global.auth.custom.CustomUserDetailsService;
+import com.trekker.global.config.redis.dao.RedisRepository;
+import com.trekker.global.config.security.filter.JwtFilter;
+import com.trekker.global.config.security.handler.CustomAccessDeniedHandler;
+import com.trekker.global.config.security.handler.CustomAuthenticationEntryPoint;
+import com.trekker.global.config.security.handler.CustomLoginSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -21,17 +30,48 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final TokenProvider tokenProvider;
+    private final RedisRepository redisRepository;
+    private final CustomUserDetailsService customUserDetailsService;
+    private final CustomLoginSuccessHandler loginSuccessHandler;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
+    /**
+     * AuthenticationManager 빈 등록
+     */
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(
+                AuthenticationManagerBuilder.class);
+        return authenticationManagerBuilder.build();
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .httpBasic(AbstractHttpConfigurer::disable) // HTTP Basic 인증을 비활성화 (JWT 방식을 사용하기 위해서)
+                .httpBasic(
+                        AbstractHttpConfigurer::disable) // HTTP Basic 인증을 비활성화 (JWT 방식을 사용하기 위해서)
                 .csrf(AbstractHttpConfigurer::disable) // CSRF 보호를 비활성화 (JWT 인증은 세션이 아닌 토큰 기반이라 필요 없음)
                 .cors(cors -> cors.configurationSource(
                         corsConfigurationSource())) // CORS 설정을 활성화하여 corsConfigurationSource()에서 상세 설정을 정의
                 .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(
                         STATELESS)) // 세션 정책을 STATELESS로 설정하여 서버에서 세션을 생성하지 않음
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                        authorizationManagerRequestMatcherRegistry
+                                .requestMatchers("/api/v1/auth/refresh").permitAll()
+                                .requestMatchers("/login/oauth2/**", "/oauth2/**").permitAll()
+                                .anyRequest().authenticated())
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(customAuthenticationEntryPoint)
+                        .accessDeniedHandler(customAccessDeniedHandler))
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(
+                                userInfo -> userInfo.userService(customUserDetailsService))
+                        .successHandler(loginSuccessHandler))
+                .addFilterBefore(new JwtFilter(tokenProvider, redisRepository),
+                        UsernamePasswordAuthenticationFilter.class)
                 .build();
-
     }
 
     @Bean

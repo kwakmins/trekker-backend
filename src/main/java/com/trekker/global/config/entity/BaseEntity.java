e0c6eb8367ccfb244947715bb6af1813b449a782
kawksung <alstjd5479@naver.com>
2024-11-24 13:17:05 +0900

    feature : BaseEntity 구현 #6

---
+++ src/main/java/com/trekker/global/config/entity/BaseEntity.java
@@ -0,0 +1,15 @@
+package com.trekker.global.config.entity;
+
+import static lombok.AccessLevel.PROTECTED;
+
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@MappedSuperclass
+public abstract class BaseEntity extends AuditBaseEntity {
+
+    private boolean isDelete;
+}

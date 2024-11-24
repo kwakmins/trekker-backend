972e1b565617cc752be9543185e8492a4badbb1a
pcb7893@naver.com <122460524+chanbyoung@users.noreply.github.com>
2024-11-24 22:05:45 +0900

    feat: 에러 코드 정의

--- src/main/java/com/trekker/global/exception/enums/ErrorCode.java
+++ src/main/java/com/trekker/global/exception/enums/ErrorCode.java
@@ -9,8 +9,16 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     //GLOBAL
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류")
-    ;
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류"),
+
+    //Security
+    ACCESS_DENIED_EXCEPTION(HttpStatus.FORBIDDEN, "필요한 접근 권한이 없습니다."),
+    ACCESS_AUTH_ENTRY_EXCEPTION(HttpStatus.UNAUTHORIZED, "유효한 자격이 없습니다."),
+
+    //JWT
+    INVALID_REFRESH_TOKEN(HttpStatus.FORBIDDEN, "유효하지 않은 리프레시 토큰입니다."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
+    AUTHENTICATION_FAILED(HttpStatus.UNAUTHORIZED, "인증에 실패했습니다.");
 
     //오류 상태코드
     private final HttpStatus httpStatus;

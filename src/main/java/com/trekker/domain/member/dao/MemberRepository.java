0aa98cd0d8aaa0165fad5b020a3a6b472d5ceb17
pcb7893@naver.com <122460524+chanbyoung@users.noreply.github.com>
2024-11-26 14:15:14 +0900

    refactor: 쿼리문 가독성을 위한 수정

--- src/main/java/com/trekker/domain/member/dao/MemberRepository.java
+++ src/main/java/com/trekker/domain/member/dao/MemberRepository.java
@@ -8,10 +8,20 @@ import org.springframework.data.repository.query.Param;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    @Query("select m from Member m join fetch m.socialProvider s where m.email =:email")
+    @Query("""
+            SELECT m 
+            FROM Member m 
+            JOIN FETCH m.socialProvider s 
+            WHERE m.email =:email 
+            """)
     Optional<Member> findByEmail(@Param("email") String email);
 
-    @Query("select m from Member m join fetch m.socialProvider s where s.provider =:provider and s.providerId =:providerId")
+    @Query("""
+            SELECT m 
+            FROM Member m 
+            JOIN FETCH m.socialProvider s 
+            WHERE s.provider =:provider AND s.providerId =:providerId
+            """)
     Optional<Member> findByProviderAndProviderId(@Param("provider") String provider,
             @Param("providerId") String providerId);
 }

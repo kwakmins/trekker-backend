516e2adcd3cd2ae221ce301e1c4bd487fec0cf68
pcb7893@naver.com <122460524+chanbyoung@users.noreply.github.com>
2024-12-03 16:04:57 +0900

    refactor: 엔드포인트 수정

--- src/main/java/com/trekker/domain/calender/api/CalendarController.java
+++ src/main/java/com/trekker/domain/calender/api/CalendarController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/calender")
+@RequestMapping("/api/v1/calendar")
 @RequiredArgsConstructor
 public class CalendarController {
 
